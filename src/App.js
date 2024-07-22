97878817f0ac9f4be373173a8e7ea3403d1f2950
Yan Victor <yanvictorsn@gmail.com>
2024-07-22 19:33:43 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
 
+import { MapContainer, TileLayer, CircleMarker, Popup, Marker } from "react-leaflet";
+import React, { useEffect, useState } from "react";
+import "leaflet/dist/leaflet.css"
+import { Icon } from "leaflet";
+import MarkerClusterGroup from 'react-leaflet-cluster';
+
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('/output_data.json')
+      .then(response => response.json())
+      .then(jsonData => setData(jsonData))
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
+  const transformedData = data.map(item => ({
+    name: item[0],
+    description: item[1],
+    latitude: parseFloat(item[3]),
+    longitude: parseFloat(item[4])
+  })).filter(item => !isNaN(item.latitude) && !isNaN(item.longitude));
+
+  const customIcon = new Icon({
+    iconUrl: "https://cdn-icons-png.flaticon.com/512/684/684908.png",
+    iconSize: [38, 38]
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true} style={{ height: "100vh", width: "100%" }}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <MarkerClusterGroup>
+        {transformedData.map((item, index) => (
+          <Marker key={index} position={[item.latitude, item.longitude]} icon={customIcon}>
+            <Popup>{item.name}</Popup>
+          </Marker>
+        ))}
+      </MarkerClusterGroup>
+    </MapContainer>
   );
 }
 
