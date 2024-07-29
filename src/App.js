ff91d854c3fcf01122e1f8fe6a2029c9d015e39d
Yan Victor <yanvictorsn@gmail.com>
2024-07-29 18:08:46 -0300

    upload new link map

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 
-import { MapContainer, TileLayer, CircleMarker, Popup, Marker } from "react-leaflet";
+import { MapContainer, TileLayer, Popup, Marker } from "react-leaflet";
 import React, { useEffect, useState } from "react";
 import "leaflet/dist/leaflet.css"
 import { Icon } from "leaflet";
@@ -52,8 +52,8 @@ function App() {
           style={{ height: '100%', width: '100%', backgroundColor: "white" }}
         >
           <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="https://www.openstreetmap.de/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.de/{z}/{x}/{y}.png"
             minZoom={2}
             maxZoom={18}
           />
